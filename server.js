7d8bffa95c6da93d708042c93bfc25d916a72ab8
H4rish55 <harishdp01@gmail.com>
2025-03-11 17:07:22 -0400

    added register page

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ app.use('/images', express.static(__dirname + 'public/images'))
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-app.get("/", (req, res) => {
+app.get("/login", (req, res) => {
     res.render("login")
 })
+
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+
 
 app.listen(3000)
