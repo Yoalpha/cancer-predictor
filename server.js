28d18902d4de2850f45de43a3bee099576f18597
yoalpha <yoyogesh27@gmail.com>
2025-03-10 21:10:26 -0400

    update

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const app = express()
 
+// Static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/images', express.static(__dirname + 'public/images'))
+
+
+// Set views
+app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("login")
 })
 
 app.listen(3000)
