2b42bae6ca36d33e12e0b35534409d951c9b3f60
yoalpha <yoyogesh27@gmail.com>
2025-03-13 03:44:47 -0400

    added styling to all the pages and connected the database

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+require('dotenv').config()
+
 const express = require("express")
+
+const { default: mongoose } = require("mongoose")
 const app = express()
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+ 
+const db = mongoose.connection
+
+db.on('error', (error)=> console.log(error))
+db.once('open', ()=> console.log('CONNECTED TO DATABASE'))
 
 // Static files
 app.use(express.static('public'))
+app.user(express.json())
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 app.use('/images', express.static(__dirname + 'public/images'))
+app.use(express.urlencoded())
 
 
 // Set views
@@ -17,13 +30,50 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get("/login", (req, res) => {
+
+
+
+
+app.route("/login")
+.get((req, res) => {
     res.render("login")
+
+})
+.post((req, res) => {
+    let username = req.body.namee
+    let password = req.body.password
+    console.log(username)
+    console.log(password)
+    res.send("hi")
 })
 
-app.get("/register", (req, res) => {
+
+
+
+
+app.route("/register")
+.get((req, res) => {
     res.render("register")
+    
 })
+.post((req, res) => {
+    let username = req.body.username
+    let email = req.body.email
+    let password = req.body.password
+    let confirm_password = req.body.confirm_password
+    let role = req.body.role
+    if(password !== confirm_password){
+        res.render("register", { msg: "PASSWORDS DO NOT MATCH" })
+
+    }
+
+
+
+    res.redirect('/login')
+})
+
+
+
 
 app.get("/patient-form", (req, res) => {
     res.render("form")
